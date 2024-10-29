bad642e0ecb2029500fa561a098a279f41db3a2f
FedotPushkin <d.sidorov54@gmail.com>
2024-10-30 00:56:34 +0400

    load from hd5f and show sample

--- loading.py
+++ loading.py
@@ -1,46 +1,96 @@
 import os
-from cTab import CTab
 import numpy as np
-from C_Truth import C_Truth
-from tabToImage import tabToImage
-
-
-def load_signatures(folder_path, im_size, load, test):
+import h5py
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from skimage.transform import resize
+from visualisation import plot_df_samples
+def load_db(folder_path, im_size, load, test):
     # List of all *.tab files in folder
     if load:
-        files = [f for f in os.listdir(folder_path) if f.endswith('.tab')]
-        if not files:
+        file_names = [f for f in os.listdir(folder_path) if f.endswith('.hdf5')]
+        if not file_names:
             raise ValueError("No .tab files found in the specified folder.")
         names, imgs, stps = [], [], []
 
-        for file_name in files:
+        for file_name in file_names:
             file_path = os.path.join(folder_path, file_name)
 
             try:
-                tab = CTab(file_path)
-                if tab is None:
-                    raise Exception('cant read tab file')
-                img, stp = tabToImage(tab, im_size)
-                if img is None or stp is None:
+                #members = h5py.File(file_path)['TRACE_DATA/DEFAULT']
+                #for m in members:
+                #    print(m)
+                with h5py.File(file_path, 'r') as h5file:
+
+                    def print_structure(name, obj):
+                        print(f"{name}: {type(obj)}")
+                        if isinstance(obj, h5py.Dataset):
+                            print(f"  Shape: {obj.shape}")
+                            print(f"  Dtype: {obj.dtype}")
+
+                    # Visit each object in the file
+                    #h5file.visititems(print_structure)
+                    data_arr = h5file['TRACE_DATA/DEFAULT/data_array'][:]
+                    samp_num = h5file['TRACE_DATA/DEFAULT/SAMP_NUM'][:]
+                    rec_x = h5file['TRACE_DATA/DEFAULT/REC_X'][:]
+                    rec_y = h5file['TRACE_DATA/DEFAULT/REC_Y'][:]
+                    samp_rate = h5file['TRACE_DATA/DEFAULT/SAMP_RATE'][:]
+                    f_break = h5file['TRACE_DATA/DEFAULT/SPARE1'][:]
+                    pivots = []
+                    pivots2 = []
+                    for idx, x in enumerate(rec_x[:4000]):
+                        if idx == 0:
+                            continue
+                        if x > rec_x[idx+1] and x > rec_x[idx-1]:
+                            pivots.append(idx)
+                        if x < rec_x[idx+1] and x < rec_x[idx-1]:
+                            pivots2.append(idx)
+                    data_splits = np.split(data_arr, pivots, axis=0)
+
+                    break_splits = np.split(f_break, pivots, axis=0)
+                    labels = [*range(len(break_splits))]
+                    df = pd.DataFrame({'2d_array': data_splits, '1d_array': break_splits, 'Label': labels})
+                    for idx, img in enumerate(df['2d_array']):
+                        #resized_img = resize(img, (im_size, im_size), anti_aliasing=True)
+                        normalized_img = img#(resized_img - resized_img.min()) / (resized_img.max() - resized_img.min())
+                        df.at[idx, '2d_array'] = normalized_img
+                        arr = df.loc[idx, '1d_array']
+                        arr = arr.astype(object)
+                        arr[arr == 0] = None
+                        arr[arr == -1] = None
+                        df.at[idx, '1d_array'] = arr
+                        #df.loc[idx, '1d_array'][df.loc[idx, '1d_array'] == -1] = None
+
+                    #df.to_hdf('data.h5', key='df', mode='w')
+                    #arr1 = df['2d_array'].to_numpy()
+                    #arr2 = df['1d_array'].to_numpy()
+                    #np.savez('my_arrays.npz', arr1=arr1, arr2=arr2)
+                    #pd.read_hdf('data.h5', key='df')
+                    plot_df_samples(df)
+                if data_splits is None:
+                    raise Exception('cant read data file')
+
+                if data_splits is None or data_splits is None:
                     raise Exception('cant create image from tab')
                 print('loaded file with id ', file_name)
-                imgs.append(img)
-                stps.append(stp)
+
                 if file_name is not None:
                     names.append(file_name)
                 else:
                     raise ValueError(f" {file_name} tab is None")
             except Exception as e:
-                print(f'Error loading file {file_name}: {e}')
+                print(f'Exception happened: Error loading file {file_name}: {e}')
                 continue
 
-        if not names or imgs is None:
-            raise ValueError("Failed to load signatures.")
+        if file_name is None:
+            raise ValueError("Failed to load db.")
         if not test:
+            pass
             #names, imgs, stps = np.array(names), np.array(imgs), np.array(stps)
-            np.save(f'sample_ids_{im_size}.npy', names)
-            np.save(f'images_{im_size}.npy', imgs)
-            np.save(f'stps_{im_size}.npy', stps)
+            #np.save(f'sample_ids_{im_size}.npy', names)
+            #np.save(f'images_{im_size}.npy', imgs)
+            #np.save(f'stps_{im_size}.npy', stps)
 
     else:
         if not os.path.exists('sample_ids.npy') or not os.path.exists('images.npy'):
@@ -52,45 +102,8 @@ def load_signatures(folder_path, im_size, load, test):
 
         if names is None or imgs is None:
             raise ValueError("Failed to load signatures.")
-    return names, imgs, stps
+    return data_splits
 
-
-def load_form_and_truth(sample_ids):
-
-    c_truth = C_Truth()
-    forms, truth = [], []
-    if c_truth.load("images/truth.txt"):
-        for s_id in sample_ids:
-            try:
-                truth_line = c_truth.get(s_id, 100)
-                if truth_line:
-                    parts = truth_line.split(' ')
-                    if len(parts) != 3:
-                        raise ValueError(f"Expected 3 elements in truth line, got {len(parts)}: {truth_line}")
-
-                    _, form, isgenuine = truth_line.split(' ')
-                    if form is not None:
-                        forms.append(form)
-                    else:
-                        raise Exception('error reading form')
-                    if isgenuine is None:
-                        raise Exception('error reading genuine/forged')
-                    elif isgenuine == 'genuine':
-                        truth.append(1)
-                    elif isgenuine == 'forged':
-                        truth.append(0)
-                    else:
-                        raise Exception(f'signature verdict must be genuine or forged, found {isgenuine}')
-                else:
-                    print("File not found in dictionary")
-                    return
-            except Exception as e:
-                print(f'error occurred, {e}')
-
-        return forms, truth
-    else:
-        print('truth file loading error')
-        return
 
 
 
