744a37d33a89733d7ad9ede46bca8f13c22a95ea
FedotPushkin <d.sidorov54@gmail.com>
2024-11-01 00:11:08 +0400

    setup datasets and model

--- nn.py
+++ nn.py
@@ -4,17 +4,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from visualisation import plothistory
-from tensorflow.keras import layers, models, regularizers
-from sklearn.model_selection import KFold
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout,  GlobalAveragePooling2D
 from sklearn.utils.class_weight import compute_class_weight
 from visualisation import show_performance_metrics, make_predictions
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import MobileNetV2
-import segmentation_models as sm
 from keras_unet_collection.models import unet_3plus_2d
 from sklearn.model_selection import train_test_split
+
 
 class Unet_NN:
 
@@ -44,14 +39,13 @@ class Unet_NN:
                              pool='max',
                              unpool='bilinear',
                              deep_supervision=True,
-                             n_labels=3,)
+                             n_labels=1,)
 
+        num_classes = 3
+        model.compile(optimizer='adam',
+                      loss='categorical_crossentropy' if num_classes > 1 else 'binary_crossentropy',
+                      metrics=['accuracy'])
 
-
-        #for layer in base_network.layers:
-        #    layer.trainable = False
-        #for layer in base_network.layers[-3:]:
-        #    layer.trainable = True
         return model
 
     def fit_to_data(self, X, y, batch_size, epochs, show_perf):
@@ -63,12 +57,16 @@ class Unet_NN:
             if not os.path.exists('models'):
                 os.makedirs('models')
 
-            dataset, val_set = self.get_datasets(X_train, y_train, X_test, y_test, batch_size)
             early_stop = keras.callbacks.EarlyStopping(monitor='val_loss',
                                                        patience=5,
                                                        verbose=1,
                                                        restore_best_weights=True
                                                        )
+            dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))\
+                .batch(batch_size).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+            val_set = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(batch_size)\
+                .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
             self.history = self.model.fit(dataset,
                                           validation_data=val_set,
                                           epochs=epochs,
@@ -85,43 +83,3 @@ class Unet_NN:
                 y_pred = make_predictions(X_test)
                 show_performance_metrics(self.y_test, y_pred)
 
-    def custom_generator(self, X1, y, batch_size, train_generator):
-        num_samples = X1.shape[0]
-        while True:
-            indices = np.random.permutation(num_samples)  # Shuffle indices
-            for start in range(0, num_samples, batch_size):
-                end = min(start + batch_size, num_samples)
-                batch_indices = indices[start:end]
-
-                # Apply augmentation to both input images
-                X1_augmented = train_generator.flow(X1[batch_indices], batch_size=batch_size, shuffle=False)
-
-                # Get the augmented images
-                X1_batch = next(X1_augmented)
-
-
-                # Yield the two augmented input images and their labels
-                yield X1_batch, y[batch_indices]
-
-    def get_datasets(self,X1_t, X2_t, y_t, X1_v, X2_v, y_v, batch_size):
-
-        im_gen = ImageDataGenerator(
-            rotation_range=10,
-            width_shift_range=0.05,
-            height_shift_range=0.05,
-            shear_range=0.01,
-            zoom_range=[0.95, 1.05],
-
-            fill_mode='nearest'
-        )
-        train_gen = self.custom_generator(X1_t, y_t, batch_size, im_gen)
-        val_gen = self.custom_generator(X1_v,  y_v, batch_size, ImageDataGenerator())
-        signature = (tf.TensorSpec(shape=(None, self.im_size, self.im_size, 2), dtype=tf.float16),
-                     tf.TensorSpec(shape=(None,), dtype=tf.float16))
-
-        train_dataset = tf.data.Dataset.from_generator(lambda: train_gen, output_signature=signature). \
-            prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-
-        validation_set = tf.data.Dataset.from_generator(lambda: val_gen, output_signature=signature). \
-            prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-        return train_dataset, validation_set
