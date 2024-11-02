306d8fadae65df34f925f646db4522cdcfab8cfb
FedotPushkin <d.sidorov54@gmail.com>
2024-11-02 21:34:39 +0400

    custom objects added for loading model
    metrics shown and sample shown

--- nn.py
+++ nn.py
@@ -5,13 +5,16 @@ import tensorflow as tf
 from tensorflow import keras
 from visualisation import plothistory
 from sklearn.utils.class_weight import compute_class_weight
-from visualisation import show_performance_metrics, make_predictions
+from tensorflow.keras.metrics import MeanIoU
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras_unet_collection.models import unet_3plus_2d
 from sklearn.model_selection import train_test_split
 from tensorflow_addons.losses import SigmoidFocalCrossEntropy
 from segmentation_models.metrics import IOUScore, Precision, Recall
 from segmentation_models.losses import DiceLoss, JaccardLoss, CategoricalFocalLoss
+import json
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
 
 
 class Unet_NN:
@@ -26,8 +29,12 @@ class Unet_NN:
         self.im_height = input_shape[1]
         self.history = None
         self.model = self.setup_unet(input_shape)
+
     @staticmethod
-    def setup_unet(input_shape):
+    def dice_loss_plus_focal_loss(y_true, y_pred):
+        return DiceLoss()(y_true, y_pred) + CategoricalFocalLoss(gamma=2.0)(y_true, y_pred)
+
+    def setup_unet(self, input_shape):
 
         model = unet_3plus_2d(
                              input_size=input_shape,
@@ -44,7 +51,7 @@ class Unet_NN:
                              deep_supervision=True,
                              n_labels=3,)
 
-        loss_fn = DiceLoss(class_weights=[1.0, 10.0, 1.0]) + CategoricalFocalLoss(gamma=2.0)
+        loss_fn = self.dice_loss_plus_focal_loss
         metrics = [IOUScore(class_indexes=[0, 1, 2]), Precision(class_indexes=[1]), Recall(class_indexes=[1])]
         model.compile(optimizer='adam',
                       loss=loss_fn,
@@ -52,7 +59,7 @@ class Unet_NN:
 
         return model
 
-    def fit_to_data(self, dataset, val_dataset, batch_size, epochs, show_perf,train_samples, test_samples):
+    def fit_to_data(self, dataset, val_dataset, batch_size, epochs, show_perf, train_samples, test_samples):
 
        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         with tf.device('/GPU:0'):
@@ -85,8 +92,53 @@ class Unet_NN:
 
             gc.collect()
         if self.history is not None:
+            with open('training_history.json', 'w') as file:
+                json.dump(self.history.history, file)
             if show_perf:
                 plothistory(self.history.history,)
-                #y_pred = make_predictions(X_test)
-                #show_performance_metrics(self.y_test, y_pred)
+
+    def make_predictions(self, val_dataset, batch_size, test_samples):
+
+
+        model_name = f'Unet3Plus_model.h5'
+        model_path = os.path.join('models/', model_name)
+        if os.path.exists(model_path):
+            model = load_model(model_path, custom_objects={
+                'dice_loss_plus_focal_loss': self.dice_loss_plus_focal_loss,
+                'iou_score': IOUScore(class_indexes=[0, 1, 2]),
+                'precision': Precision(class_indexes=[1]),
+                'recall': Recall(class_indexes=[1])
+                # other custom objects can be added here
+            })
+        else:
+            raise FileNotFoundError(f"Model file {model_name} not found.")
+
+
+        # IoU calculation
+        #iou_metric = MeanIoU(num_classes=3)
+        #for x, y in val_dataset:
+        #    y_pred = model.predict(x)
+        #    y_pred = np.argmax(y_pred, axis=-1)  # For multi-class
+        #    iou_metric.update_state(y, y_pred)
+
+        #iou_score = iou_metric.result().numpy()
+        X_test = []
+        y_test = []
+        num_batches = 1
+        for x_batch, y_batch in val_dataset.take(num_batches):
+            X_test.append(x_batch.numpy())  # Convert tensor to numpy array
+            y_test.append(y_batch.numpy())
+        #print(f"Test IoU: {iou_score}")
+
+        sample_image = X_test[0]  # Use one sample image
+        prediction = model.predict(sample_image)
+        #evaluation_results = model.evaluate(val_dataset, steps=test_samples // batch_size, return_dict=True)
+        # y_pred = model.predict(val_dataset, steps=test_samples // batch_size)
+        #loss = evaluation_results['loss']
+        #print(f'loss {loss}')
+        #for metric_name, metric_value in evaluation_results.items():
+        #    print(f"{metric_name}: {metric_value}")
+        return y_test[0], prediction[0]
+
+
 
