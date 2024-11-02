e6f025346daed47175fcf95e60fbc6907bcf9550
FedotPushkin <d.sidorov54@gmail.com>
2024-11-02 04:41:09 +0400

    chunks to support big files
    and bug fixed in line 158

--- loading.py
+++ loading.py
@@ -12,8 +12,10 @@ from build_data import create_tf_dataset_from_hdf5
 #@memory_profiler.profile
 
 
-def drop_train_data_to_file(masks, traces_img):
-    with h5py.File('train_dataset', 'a') as h5file:
+def drop_train_data_to_file(masks, traces_img, train_shape):
+
+    with h5py.File('train_dataset.hdf5', 'a') as h5file:
+
         # Check if the datasets already exist
         if 'masks' in h5file:
             # If exists, resize the dataset and add new data
@@ -21,7 +23,8 @@ def drop_train_data_to_file(masks, traces_img):
             h5file['masks'][-masks.shape[0]:] = masks  # Add new data at the end
         else:
             # Create the dataset if it doesn't exist
-            h5file.create_dataset('masks', data=masks)
+            h5file.create_dataset('masks', data=masks,
+                                  maxshape=(None, train_shape[0], train_shape[1], 1), chunks=True)
 
         if 'traces_img' in h5file:
             # If exists, resize the dataset and add new data
@@ -29,17 +32,18 @@ def drop_train_data_to_file(masks, traces_img):
             h5file['traces_img'][-traces_img.shape[0]:] = traces_img  # Add new data at the end
         else:
             # Create the dataset if it doesn't exist
-            h5file.create_dataset('traces_img', data=traces_img)
+            h5file.create_dataset('traces_img', data=traces_img,
+                                  maxshape=(None, train_shape[0], train_shape[1], 1), chunks=True)
 
     print("New arrays added to HDF5 file successfully.")
 
 
-def load_db(folder_path, train_shape, load, test,batch_size):
-    chunk_size = 1100000
+def load_db(folder_path, train_shape, load, test, batch_size):
+    chunk_size = 11000
     # List of all *.hdf5 files in folder
     if load:
 
-        file_names = [f for f in os.listdir(folder_path) if f.endswith('.hdf5')]
+        file_names = [f for f in os.listdir(folder_path) if (f.endswith('.hdf5') or (f.endswith('.hdf')))]
         if not file_names:
             raise ValueError("No .hdf5 files found in the specified folder.")
 
@@ -58,7 +62,7 @@ def load_db(folder_path, train_shape, load, test,batch_size):
                     samp_rate_arr = np.array(samp_rate_arr).flatten()
                     samp_num_arr = np.array(samp_num_arr).flatten()
 
-                    #plot_train_sample(data_arr[:171], f_break[:171]*0.5)
+                    plot_train_sample(data_arr[:171], f_break[:171]*0.5)
                     if not np.all(samp_rate_arr == samp_rate_arr[0]):
                         raise ValueError('samp rate not constant')
                     if not np.all(samp_num_arr == samp_num_arr[0]):
@@ -85,7 +89,8 @@ def load_db(folder_path, train_shape, load, test,batch_size):
                     max_width_f = 0
                     #widths_f = []
                     skipped = []
-                    for idx, arr in tqdm(enumerate(first_break_lines), total=len(first_break_lines),
+                    len_f = len(first_break_lines)
+                    for idx, arr in tqdm(enumerate(first_break_lines), total=len_f,
                                          desc="Interpolating missing labels"):
                         n_lines = len(arr)
                         if n_lines == 0:
@@ -95,11 +100,12 @@ def load_db(folder_path, train_shape, load, test,batch_size):
                         arr[np.isin(arr, [0, -1])] = np.nan
 
                         mask = ~np.isnan(arr)
-                        if mask.any():
+                        if mask.any() and max(arr) != min(arr):
                             x = np.arange(n_lines)
                             first_break_lines[idx] = np.interp(x, x[mask], arr[mask]).astype(int)
                             del x, mask
                         else:
+
                             skipped.append(idx)
                             print(f'skipped  line {idx}, all nans')
                             continue
@@ -109,19 +115,21 @@ def load_db(folder_path, train_shape, load, test,batch_size):
                     #plot_df_samples(df)
                     end_time = time.time()
                     print(f"Time taken to interpolate: {(end_time - start_time)/60:.1f} minutes")
-                    print(f"Skipped {len(skipped)} lines : {len(skipped)/n_lines:.1f} %")
+                    print(f"Skipped {len(skipped)} lines : {len(skipped)/len_f:.1f} %")
                     shapes = []
                     filtered_arr1 = [item for idx, item in enumerate(first_break_lines) if idx not in skipped]
 
                     filtered_arr2 = [item for idx, item in enumerate(traces_img) if idx not in skipped]
-
+                    if len(filtered_arr1)==0 or len(filtered_arr2)==0:
+                        continue
                     coef = (1000 / samp_rate).astype(np.float64)
                     coef_y = train_shape[1] / ds_height
                     masks = []
-                    #plot_train_sample(filtered_arr2[0], filtered_arr1[0] * 0.5)
+                    if len(filtered_arr1) > 0 and len(filtered_arr2) > 0:
+                        plot_train_sample(filtered_arr2[0], filtered_arr1[0] * 0.5)
 
                     for idx, arr in tqdm(enumerate(filtered_arr1), desc="Creating masks"):
-                        if arr.shape[0] == 0 or arr.shape[0] > max_width_f:
+                        if arr.shape[0] == 0 or arr.shape[0] > max_width_f or (np.array(arr)<0).any():
                             raise ValueError('array larger then max_width')
                         elif 0 < arr.shape[0] <= max_width_f:
 
@@ -143,18 +151,20 @@ def load_db(folder_path, train_shape, load, test,batch_size):
 
                                 y_line_resized = np.concatenate((y_line_resized, np.zeros(num_zeros_resized, dtype=np.float32)))
                                 filtered_arr1[idx] = (y_line_resized * coef * coef_y).astype(int)
+                                #del y_line_resized
                             else:
 
                                 if len(arr) <= train_shape[0]:
-                                    num_zeros_resized = train_shape[0] - len(arr)
-                                    filtered_arr1[idx] = (np.concatenate((arr, np.zeros(num_zeros_resized, dtype=np.float32))) * coef * coef_y).astype(int)
+                                    num_zeros_resized = max(train_shape[0] - len(arr), 0)
+                                    filtered_arr1[idx] = np.clip(np.concatenate((arr * coef * coef_y, np.zeros(num_zeros_resized, dtype=np.float32)))
+                                                                  ).astype(np.int32)
                                 else:
                                     filtered_arr1[idx] = (arr[:train_shape[0]] * coef * coef_y).astype(int)
                                     #raise ValueError('arr longer then max_width_f')
                         else:
                             raise ValueError('arr longer then max_width_f')
 
-                        min_yr = min(y_line_resized)
+                        min_yr = min(filtered_arr1[idx])
                         if min_yr < 0:
                             raise ValueError('negative ')
                         y_line_resized = filtered_arr1[idx]
@@ -174,7 +184,7 @@ def load_db(folder_path, train_shape, load, test,batch_size):
                                 raise ValueError('mask array out of range')
 
                         masks.append(mask)
-                        if len(masks) == 6:
+                        if len(masks) == 6 and 0:
                             show_mask_samples(masks, [*range(6)])
                         del mask
 
@@ -209,12 +219,12 @@ def load_db(folder_path, train_shape, load, test,batch_size):
                             img -= img_min
                             img /= (img_max - img_min)
                             filtered_arr2[idx] = img
-                    shape_arr0, shape_arr1 = [], []
-                    for f in filtered_arr2:
-                        shape_arr0.append(f.shape[0])
-                        shape_arr1.append(f.shape[1])
-                    print('before reshape', max(shape_arr0), min(shape_arr0), np.mean(shape_arr0))
-                    print(max(shape_arr1), min(shape_arr1), np.mean(shape_arr1))
+                    #shape_arr0, shape_arr1 = [], []
+                    #for f in filtered_arr2:
+                    #    shape_arr0.append(f.shape[0])
+                    #    shape_arr1.append(f.shape[1])
+                    #print('before reshape', max(shape_arr0), min(shape_arr0), np.mean(shape_arr0))
+                    #print(max(shape_arr1), min(shape_arr1), np.mean(shape_arr1))
                     traces_img = np.reshape(filtered_arr2, (len(filtered_arr2), train_shape[0], train_shape[1], 1))
                     masks = np.reshape(masks, (len(masks), train_shape[0], train_shape[1], 1))
                     shape_arr0, shape_arr1 = [], []
@@ -225,12 +235,12 @@ def load_db(folder_path, train_shape, load, test,batch_size):
                     print(max(shape_arr1), min(shape_arr1), np.mean(shape_arr1))
                     del first_break_lines, filtered_arr2
                     gc.collect()
-                    plot_train_samples(traces_img[:36], masks[:36], train_shape)
+                    #plot_train_samples(traces_img[:36], masks[:36], train_shape)
                     print('saving to array, this will take a while')
                     if masks is None or traces_img is None:
                         raise Exception('cant create image from trace')
 
-                    drop_train_data_to_file(masks=masks, traces_img=traces_img)
+                    drop_train_data_to_file(masks=masks, traces_img=traces_img, train_shape=train_shape)
                     del masks, traces_img
                     end_time = time.time()
                     print(f"Time taken to save: {(end_time - start_time) / 60:.1f} minutes")
@@ -249,7 +259,7 @@ def load_db(folder_path, train_shape, load, test,batch_size):
 
     if not os.path.exists('train_dataset.hdf5'):
         raise FileNotFoundError('Sample files not found. Set load=True to generate them.')
-    dataset, val_dataset, train_samples, test_samples = create_tf_dataset_from_hdf5('test_dataset',
+    dataset, val_dataset, train_samples, test_samples = create_tf_dataset_from_hdf5('train_dataset.hdf5',
                                                        batch_size=batch_size,
                                                        chunk_size=chunk_size,
                                                        train_ratio=0.8,
